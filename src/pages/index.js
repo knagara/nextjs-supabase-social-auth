c8b4d08fc6dc75d1f04c4e846bc4113de5905c68
knagara <nagara.keita@gmail.com>
2023-01-20 22:39:05 +0900

    supabase

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useState, useEffect } from "react";
+import supabase from "../utils/supabase";
 
 export default function Home() {
+  const [testData, setTestData] = useState("");
+
+  // Supabaseのtestテーブルに入っているテストデータを1つ取得
+  async function fetchTestData() {
+    let { data, error } = await supabase
+      .from("test")
+      .select("*")
+      .limit(1)
+      .single();
+    setTestData(data.text);
+  }
+
+  useEffect(() => {
+    fetchTestData();
+  }, []);
+
   return (
     <>
-      <main></main>
+      <main>
+        <div>
+          <h2>Supabaseのtestテーブルに入っているテストデータを1つ取得</h2>
+          <p>{testData}</p>
+        </div>
+        <div>
+          <h2>SupabaseによるGoogleログイン</h2>
+          <div></div>
+        </div>
+      </main>
     </>
   );
 }
